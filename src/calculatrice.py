0c1bf2d05185ed720fb25e32c100376f0fc4e297
SeddikiElmehdi <6383672@bdeb.qc.ca>
2026-03-03 13:17:48 -0500

    Update calculatrice.py

--- src/calculatrice.py
+++ src/calculatrice.py
@@ -1,1 +1,14 @@
+def division(a: float, b: float) -> float:
+    if b == 0:
+        raise ZeroDivisionError("Division par zéro")
+    return float(a / b)
 
+def puissance(a: float, n: int) -> float:
+    if n < 0:
+        raise ValueError("L'exposant doit être positif")
+    return float(a ** n)
+
+def moyenne(notes: list[float]) -> float:
+    if not notes:
+        raise ValueError("La liste est vide")
+    return float(sum(notes) / len(notes))
